cfc5cb0999a5addfdf99730c50035c372ff959a0
vinapro <vinapro91@gmail.com>
2022-05-14 15:38:10 -0300

    condicionais

---
+++ condicionais/if.php
@@ -0,0 +1,14 @@
+<?php
+
+$a = 11;
+$b = 21;
+
+if ($a > $b) {
+    echo "Realmente A e maior do que B <br>";
+} elseif ($b == 20) {
+    echo "B e igual a 20";
+} elseif ($a == 10) {
+    echo "A e igual a 10";
+} else {
+    echo "Nenhuma condicao e verdadeira";
+}
